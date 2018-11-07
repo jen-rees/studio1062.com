555fa33e15da7cee62b7d1097fabb09f075411d9
Jenifer Rees <jen@studio1062.com>
2018-11-06 22:29:13 -0800

    php

---
+++ php/footer.php
@@ -0,0 +1,58 @@
+<footer class="futura">
+
+    <div id="footer-img"><a href="about.php"><img src="assets/jeniferFitch.jpg" alt="Jenifer Fitch Web and Graphic Designer"><a></div>
+
+    <div id="footer-text">
+        <div id="social">
+           <p><a href="https://www.linkedin.com/in/jeniferfitch">LinkedIn/JeniferFitch</a></p>
+        
+        <ul>   <!-- <li><a class="soc-email" href="mailto:contact@jeniferfitch.com?subject=Message from jeniferfitch.com"></a></li> --> </ul>
+    
+        </div>
+        <!-- <li><a href="assets/jeniferRees_resume_2018.pdf">Resume</a></li>
+        <li>(707) 631-6934</li>
+        <li><a href="mailto:jen@studio1062.com?subject=Message from studio1062.com">jen@studio1062.com</a></li>
+        <li><a href="http://linkedin.com/in/jeniferfitch" target="_blank">linkedin.com/in/jeniferfitch</a></li> -->
+
+        </ul>
+        <div class="clear"></div>
+    
+
+
+
+    <div id="twitter-foot">
+        <a href="https://twitter.com/JeniferFitch" class="twitter-follow-button" data-show-count="false">Follow
+            @JeniferFitch</a>
+        <script>!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = "//platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); } }(document, "script", "twitter-wjs");</script>
+    </div>
+
+
+    <p class="footer-contact"><a href="about.php">Contact &amp; About</a></p><p><a href="assets/jeniferRees_resume_2018.pdf">Resume
+            (PDF)</a>  |  <a href="assets/jfitch_portfolio.pdf">Full Portfolio (PDF)</a></p>
+
+
+    </div>
+    <div class="clear"></div>
+    <!-- <div id="copy">&copy; All Rights Reserved <?php echo date('Y'); ?></div> -->
+
+
+    <div class="clear"></div>
+</footer>
+
+<script>
+    var navigation = responsiveNav(".nav-collapse", {
+        animate: true,                    // Boolean: Use CSS3 transitions, true or false
+        transition: 284,                  // Integer: Speed of the transition, in milliseconds
+        label: "Menu",                    // String: Label for the navigation toggle
+        insert: "after",                  // String: Insert the toggle before or after the navigation
+        customToggle: "",                 // Selector: Specify the ID of a custom toggle
+        closeOnNavClick: false,           // Boolean: Close the navigation when one of the links are clicked
+        openPos: "relative",              // String: Position of the opened nav, relative or static
+        navClass: "nav-collapse",         // String: Default CSS class. If changed, you need to edit the CSS too!
+        navActiveClass: "js-nav-active",  // String: Class that is added to <html> element when nav is active
+        jsClass: "js",                    // String: 'JS enabled' class which is added to <html> element
+        init: function () { },               // Function: Init callback
+        open: function () { },               // Function: Open callback
+        close: function () { }               // Function: Close callback
+    });
+</script>
---
+++ php/header.php
@@ -0,0 +1,40 @@
+<header>
+	
+    	<hgroup>
+        	 <a href="index.php"><h1 id='logo'>Jenifer Fitch Web &amp; Graphic Design</h1></a>
+
+        
+        <!--<div id="header-right">
+        	
+            <div id="social">
+            	<ul>
+               		
+                	<li><a class="soc-linkedin" href="https://www.linkedin.com/in/jeniferfitch" target="_blank"></a></li>
+                    <li><a class="soc-twitter" href="https://twitter.com/jeniferfitch" target="_blank"></a></li>
+                    <li><a class="soc-email" href="mailto:contact@jeniferfitch.com?subject=Message from jeniferfitch.com"></a></li>
+                   
+                    
+                </ul>
+            </div>
+           
+           <!-- close social 
+            
+            	   
+                
+        </div> <!--close header-right 
+        
+        -->
+        
+        <div id="header-right-mobile">
+            	<ul>
+                   	<li><a href="about.php">ABOUT/CONTACT</a></li>
+                    <li><a href="assets/jfitch_portfolio.pdf">FULL PORTFOLIO</a></li>
+                </ul>
+            </div>	
+            <div class="clear"></div>
+
+    </hgroup>
+	
+</header> 
+    
+    
